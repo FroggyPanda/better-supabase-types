faad4717640ef34f820f9e3f4bcfd4be4f2089c1
Janeene Beeforth <dawnmist@users.noreply.github.com>
2023-05-06 14:41:26 +1000

    Fix schema name in type definitions

--- src/generate.ts
+++ src/generate.ts
@@ -1,7 +1,13 @@
 import fs from 'fs';
 import { getTablesProperties, prettierFormat, toPascalCase } from './utils';
 
-export async function generate(input: string, output: string, prettierConfigPath?: string, schemas: string[] = ['public'], makeSingular: boolean = false) {
+export async function generate(
+  input: string,
+  output: string,
+  prettierConfigPath?: string,
+  schemas: string[] = ['public'],
+  makeSingular: boolean = false
+) {
   const exists = fs.existsSync(input);
 
   if (!exists) {
@@ -9,25 +15,31 @@ export async function generate(input: string, output: string, prettierConfigPath
     return;
   }
 
-  const tablesProperties = schemas.flatMap((schema) => getTablesProperties(input, schema));
   const types: string[] = [];
 
-  for (const table of tablesProperties) {
-    const tableName = table.getName();
-    const tableNameType = toPascalCase(tableName, makeSingular);
+  for (const schema of schemas) {
+    const tablesProperties = getTablesProperties(input, schema);
 
-    types.push(
-      `export type ${tableNameType} = Database['public']['Tables']['${tableName}']['Row'];`,
-      `export type Insert${tableNameType} = Database['public']['Tables']['${tableName}']['Insert'];`,
-      `export type Update${tableNameType} = Database['public']['Tables']['${tableName}']['Update'];`,
-      '\n'
-    );
+    for (const table of tablesProperties) {
+      const tableName = table.getName();
+      const tableNameType = toPascalCase(tableName, makeSingular);
+
+      types.push(
+        `export type ${tableNameType} = Database['${schema}']['Tables']['${tableName}']['Row'];`,
+        `export type Insert${tableNameType} = Database['${schema}']['Tables']['${tableName}']['Insert'];`,
+        `export type Update${tableNameType} = Database['${schema}']['Tables']['${tableName}']['Update'];`,
+        '\n'
+      );
+    }
   }
 
   const fileContent = fs.readFileSync(input, 'utf-8');
   let updatedFileContent = fileContent + '\n' + types.join('\n') + '\n';
   if (prettierConfigPath) {
-    updatedFileContent = await prettierFormat(updatedFileContent, prettierConfigPath);
+    updatedFileContent = await prettierFormat(
+      updatedFileContent,
+      prettierConfigPath
+    );
   }
 
   fs.writeFileSync(output, updatedFileContent);
